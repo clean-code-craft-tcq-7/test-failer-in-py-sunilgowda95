dc147efee1859ca6fbafd1d246d8fd8c3b504e15
hus6kor <sunilgowda95@gmail.com>
2022-12-01 21:43:08 +0530

    alert_failure_count undefined error resolved

--- alerter.py
+++ alerter.py
@@ -6,15 +6,13 @@ alertFailureCount = 0
 def sendAlertInCelcius(farenheit):
     returnCode = alerterSendNetworkStub(getCelciusFromFarenheit(farenheit))
     if returnCode != 200:
-        # non-ok response is not an error! Issues happen in life!
-        # let us keep a count of failures to report
-        # However, this code doesn't count failures!
-        # Add a test below to catch this bug. Alter the stub above, if needed.
+        # non-ok response is not an error! Issues happen in life!,  let us keep a count of failures to report
+        # However, this code doesn't count failures!, Add a test below to catch this bug. Alter the stub above, if needed.
         assert returnCode == 500, f'unexpected failure code {returnCode} from network stub. Expected 500 for non-ok'
         global alertFailureCount
         alertFailureCount += 0  
 
 sendAlertInCelcius(400.5)
 sendAlertInCelcius(303.6)
-print(f'{alert_failure_count} alerts failed.')
+print(f'{alertFailureCount} alerts failed.')
 assert alertFailureCount > 0, f'alert fail count not incremented {alertFailureCount} times.'
